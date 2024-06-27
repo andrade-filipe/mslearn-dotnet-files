e6d97796cdbdcd96a5ca14cda607c6d279ad2c8a
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2024-06-27 16:33:25 -0300

    Extracting .json files, step 2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿using System.IO;
 using System.Collections.Generic;
 
-var salesFiles = FindFiles("stores");
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+var salesFiles = FindFiles(storesDirectory);
 
-foreach (var file in salesFiles)
-{
+foreach (var file in salesFiles){
     Console.WriteLine(file);
 }
 
@@ -17,8 +18,8 @@ IEnumerable<string> FindFiles(string folderName)
     foreach (var file in foundFiles)
     {
         // The file name will contain the full path, so only check the end of it
-        if (file.EndsWith("sales.json"))
-        {
+        var extension = Path.GetExtension(file);
+        if (extension == ".json"){
             salesFiles.Add(file);
         }
     }
